06b4abca0582f641d29d6efc31e51447c4c223f8
baejinho05 <baejinho05@eloicube.com>
2021-02-18 23:58:42 +0900

    feat : Todolist 추가

---
+++ Module/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Wietility.Module { 
+
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
